0cc6512adc9a18dc26199d139304563572647145
unknown <khayatiadel10@gmail.com>
2024-12-23 14:21:00 +0100

    new apis

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const connectDb = require('./config/dataBase.js');
 
 const reviewRouter = require("./routes/reviewRoute.js")
 const authRouter = require("./routes/authRouter.js")
+const galleryRouter = require("./routes/galleryRouter.js")
+
 
 
 app.use(express.json())
@@ -16,10 +18,13 @@ connectDb();
 
 app.use("/api/v1/review", reviewRouter)
 app.use("/api/v1/auth", authRouter)
+app.use("/api/v1/gallery", galleryRouter )
+
 
 app.get('/', (req, res) => {
   res.send('Hello brother');
 });
+
 
 app.listen(process.env.PORT, (error) => {
   error ? console.log(error) : console.log(`Server is Running on Port ${process.env.PORT}`);
