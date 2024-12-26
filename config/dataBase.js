2ffa82934fb32d6b6dd509f8a76f0934536fa24e
Fahim Montasir Turza <turzacse@gmail.com>
2024-12-26 21:34:44 +0600

    Re-build the DB connections

--- config/dataBase.js
+++ config/dataBase.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDb = async()=>{
    try {
-const connect = await mongoose.connect('mongodb+srv://alekho:z0Bo3NgCnGakffkR@cluster0.hlnht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+// const connect = await mongoose.connect('mongodb+srv://alekho:z0Bo3NgCnGakffkR@cluster0.hlnht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+const connect = await mongoose.connect('mongodb+srv://ALEKHO1:kI5TEh8smB2yWZ9y@cluster0.fvl5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log(`DataBase is successfully connected on ${connect.connection.host}`);
    } catch (error) {
     console.error('Error connecting to the database:', error.message);
